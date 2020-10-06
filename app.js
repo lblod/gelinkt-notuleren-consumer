1ca3f0e294aca0af13987b9a7da3a607dfa99d2b
Dries Hugaerts <dries.hugaerts@live.be>
2020-10-06 14:10:14 +0200

    created database check + ingest interval.

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 import { app, errorHandler } from 'mu';
-import { SERVICE_NAME, SERVICE_VERSION } from './configuration';
+import fetch from 'node-fetch';
+import { INGEST_INTERVAL, SERVICE_NAME, SERVICE_VERSION } from './statics';
+import { waitForDatabase } from './lib/database';
 
-app.get('/', function( req, res ) {
+waitForDatabase().then(async () => {
+  // TODO if there where running task, we assume they failed mid processing
+  if (INGEST_INTERVAL > 0) {
+    // If an ingest interval was given, start ingesting at the given interval
+    (function ingest() {
+      console.log(`Executing scheduled ingestion at ${new Date().toISOString()}`);
+      fetch('http://localhost/ingest/', {method: 'POST'});
+      setTimeout(ingest, INGEST_INTERVAL)
+    })();
+  }
+});
+
+app.get('/', function(req, res) {
   res.send(`Hello, you have reached ${SERVICE_NAME} v${SERVICE_VERSION}! I'm doing just fine ^^`);
+});
+
+app.post('/ingest', async function(req, res, next) {
+  console.log('creating a sync-task');
+  // TODO Start by creating a sync-task
+
+  // TODO retrieve the first sync-task
+  //  - process this sync-task
+  //  - if success, set to done
+  //  - if failure, set failure
 });
 
 app.use(errorHandler);
--- configuration.js
+++
@@ -1,9 +0,0 @@
-
-// package.json variables
-const SERVICE_NAME = process.env.npm_package_name;
-const SERVICE_VERSION = process.env.npm_package_version;
-
-export {
-  SERVICE_NAME,
-  SERVICE_VERSION,
-};
